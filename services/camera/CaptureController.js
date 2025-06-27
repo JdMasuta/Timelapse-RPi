8c4d59426162693e78e379cf120e64f4738ea223
JdMasuta <jdmeesey@gmail.com>
2025-06-27 07:25:50 -0600

    adding font?

--- services/camera/CaptureController.js
+++ services/camera/CaptureController.js
@@ -61,7 +61,7 @@ class CaptureController {
     const filepath = path.join(this.outputDir, imageFilename);
     const font_size = 45; // Adjust font size as needed
 
-    const cmd = `fswebcam -r ${resolution} "${filepath}" --timestamp ${timestamp} --title "${imageFilename}" --font ":${font_size}"`;
+    const cmd = `fswebcam -r ${resolution} "${filepath}" --timestamp ${timestamp} --title "${imageFilename}" --font "sans-serif:${font_size}"`;
 
     Logger.info("CaptureController", "Executing capture command", {
       cmd,
